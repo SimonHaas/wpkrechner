1a856993e77190bae18e662004946ff3de964e18
Simon Haas <me@simonhaas.eu>
2021-09-30 16:35:01 +0200

    altes Zeug aus disk entfernt

--- library/dist/LoanLimitToDepotValue.d.ts
+++
@@ -1,7 +0,0 @@
-import { Calculator } from "./Calculator";
-import { Snapshot } from "./Snapshot";
-export declare class LoanLimitToDepotValue implements Calculator {
-    title: string;
-    description: string;
-    static value(snapshot: Snapshot): number;
-}
