0a46975e461dc672ff189af31908141904a91154
Simon Haas <me@simonhaas.eu>
2021-10-13 14:14:57 +0200

    kommentar

--- wpk/src/components/Inputs.jsx
+++ wpk/src/components/Inputs.jsx
@@ -4,6 +4,7 @@ import EssentialInputs from './EssentialInputs'
 export default function Inputs() {
         return (
         <div className="eingabenBox">
+            {/* Beides in eine Form */}
             <EssentialInputs />
             <AssetClasses />
         </div>
