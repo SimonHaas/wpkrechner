172cf0c966adaa8540c424e1bfdd91b7e250fad4
Simon Haas <me@simonhaas.eu>
2021-12-02 13:00:17 +0100

    Artefakt entfernt

--- library/dist/Calculator.js
+++ library/dist/Calculator.js
@@ -17,6 +17,4 @@ exports.Calculator = Calculator;
 Calculator.calculations = {
     'Beleihungsquote': new Calculation('Beleihungsquote', 'das Verhältnis vom Kreditrahmen zum Depotwert', (snapshot) => { return snapshot.creditLine / snapshot.volume; }),
     'Kreditbeanspruchung': new Calculation('Kreditbeanspruchung', 'wie weit der Beleihungswert ausgenutzt ist', (snapshot) => { return snapshot.balance / snapshot.creditLine * -1; }),
-    /*test*/
-    'Verfügbarer Betrag': new Calculation('Verfügbarer Betrag', 'Um wie viel der Kredit noch genutzt werden kann', (snapshot) => {return snapshot.creditLine * snapshot.balance}),
 };
