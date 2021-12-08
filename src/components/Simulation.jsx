e5368771373ddfe26448d6773cfcbff2772320c0
Simon Haas <me@simonhaas.eu>
2021-12-08 12:08:22 +0100

    console log

--- src/components/Simulation.jsx
+++ src/components/Simulation.jsx
@@ -11,9 +11,9 @@ import { Calculator, Snapshot } from "wpk";
 
 export default function Simulation() {
 
-  let snapshot = new Snapshot(new Date(), -800, 1000, 2000, 2,9)
-    let simulationResult = Calculator.siumulate(snapshot, {'volume': 100}, 'handel')
-    console.log({simulationResult})
+  let snapshot = new Snapshot(new Date(), -800, 1000, 2000, 2, 9)
+  let simulationResult = Calculator.siumulate(snapshot, { 'volume': 100 }, 'handel')
+  console.log({ simulationResult })
 
   return (
     <Router>
