69b10052890a3041d7153ed78ef0efc29c0add7a
Simon Haas <simon.haas@vema-eg.de>
2021-12-28 10:20:29 +0000

    snapshot als App-state

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Navigation from './components/Navigation'
+import Information from './components/Information'
+import Dashboard from './components/Dashboard'
+import Rechner from './components/Rechner'
+import Simulation from './components/Simulation'
+import { useState } from 'react'
+import { Snapshot } from 'wpk'
+
+function App() {
+  const [snapshot, setSnapshot] = useState<Snapshot>(
+    new Snapshot(new Date(), 0, 0, 0, 0)
+  );
+
+  const saveSnapshot = (e: any) => {
+    e.preventDefault();
+    localStorage.setItem("" + Date.now(), JSON.stringify(snapshot));
+  };
+
+  const updateSnapshot = (field: string, value: string) => {
+    let newSnapshot = Snapshot.fromJson(JSON.stringify(snapshot))
+    switch (field) {
+      case 'date':
+        newSnapshot.date = new Date(value)
+        setSnapshot(newSnapshot)
+        break
+      case 'volume':
+        newSnapshot.volume = +value
+        setSnapshot(newSnapshot)
+        break
+      case 'creditLine':
+        newSnapshot.creditLine = +value
+        setSnapshot(newSnapshot)
+        break
+      case 'balance':
+        newSnapshot.balance = +value
+        setSnapshot(newSnapshot)
+        break
+      case 'interestRate':
+        newSnapshot.interestRate = +value
+        setSnapshot(newSnapshot)
+        break
+    }
+  }
+  
+  return (
+    <Router>
+      <Navigation />
+      <div className='main'>
+        <Header />
+        <div className="mainpage">
+          <Route path='/' exact component={Information} />
+          <Route path='/dashboard' component={Dashboard} />
+          <Route path='/rechner' render={() => <Rechner onSubmit={saveSnapshot} onChange={updateSnapshot} snapshot={snapshot} />} />
+          <Route path='/simulation' component={Simulation} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
--- src/components/Rechner.tsx
+++ src/components/Rechner.tsx
@@ -1,49 +1,17 @@
-import { useState } from "react";
 import Kennzahlen from "./Kennzahlen";
 import Inputs from "./Inputs";
 import { Snapshot } from "wpk";
+import { FormEventHandler } from "react";
 
-export default function Rechner() {
-  const [snapshot, setSnapshot] = useState<Snapshot>(
-    new Snapshot(new Date(), 0, 0, 0, 0)
-  );
-
-  const saveSnapshot = (e: any) => {
-    e.preventDefault();
-    localStorage.setItem("" + Date.now(), JSON.stringify(snapshot));
-  };
-
-  const updateSnapshot = (field: string, value: string) => {
-    let newSnapshot = Snapshot.fromJson(JSON.stringify(snapshot))
-    switch (field) {
-      case 'date':
-        newSnapshot.date = new Date(value)
-        setSnapshot(newSnapshot)
-        break
-      case 'volume':
-        newSnapshot.volume = +value
-        setSnapshot(newSnapshot)
-        break
-      case 'creditLine':
-        newSnapshot.creditLine = +value
-        setSnapshot(newSnapshot)
-        break
-      case 'balance':
-        newSnapshot.balance = +value
-        setSnapshot(newSnapshot)
-        break
-      case 'interestRate':
-        newSnapshot.interestRate = +value
-        setSnapshot(newSnapshot)
-        break
-    }
-    console.log('updateSnapshot', JSON.stringify(newSnapshot))
-  }
-
+export default function Rechner(props: {
+  onSubmit: FormEventHandler<HTMLFormElement> | undefined;
+  onChange: (field: string, value: string) => void;
+  snapshot: Snapshot
+}) {
   return (
     <div className="mainPage-rechner">
-      <Inputs onSubmit={saveSnapshot} onChange={updateSnapshot} />
-      <Kennzahlen snapshot={snapshot} />
+      <Inputs onSubmit={props.onSubmit} onChange={props.onChange} />
+      <Kennzahlen snapshot={props.snapshot} />
     </div>
   );
 }
