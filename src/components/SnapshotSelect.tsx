2dd242c711dd4658e23bdf54ccd16b7b9652fd7e
Simon Haas <me@simonhaas.eu>
2022-01-02 13:44:39 +0100

    select Snapshot

--- src/components/SnapshotSelect.tsx
+++ src/components/SnapshotSelect.tsx
@@ -1,16 +1,26 @@
 import Select from 'react-select'
+import { Snapshot } from 'wpk';
 
 function SnapshotSelect(props: {
   setSnapshot: React.Dispatch<React.SetStateAction<any>>;
 }) {
-  const options = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' }
-  ]
+  const savedSnapshots = JSON.parse(localStorage.getItem('snapshots') || '[]')
+
+  type OptionType = {label: string, value: string}
+
+  let options: OptionType[] = []
+  savedSnapshots.forEach((snapshotObject: object) => {
+    let snapshot: Snapshot = Snapshot.fromJson(JSON.stringify(snapshotObject))
+    options.push({value: JSON.stringify(snapshot), label: new Date(snapshot.date).toLocaleDateString() + ' Kontostand: ' + snapshot.balance})
+  });
+
   return (
     <div className="dropdown-daten">
-      <Select options={options} />
+      <Select options={options} onChange={selectedOption => {
+        if (selectedOption) {
+          props.setSnapshot(Snapshot.fromJson(selectedOption.value))
+        }
+      }} />
     </div>
   );
 }
