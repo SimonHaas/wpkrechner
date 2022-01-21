b3c31983d3e93448f65e3e2a05c968605740da78
Mihrace Ertem <ertemmi76118@th-nuernberg.de>
2022-01-22 00:57:27 +0100

    anlageklassen anzeige auf simulationsseite verbessert

--- src/components/AssetClassSelect.tsx
+++ src/components/AssetClassSelect.tsx
@@ -1,21 +1,26 @@
-import Select from 'react-select';
-import { OptionType } from './Inputs';
+import Select from "react-select";
+import { OptionType } from "./Inputs";
 
 export default function AssetClassesSelect(props: {
-    options: OptionType[];
-    selectAssetClass: (index: number) => void;
+  options: OptionType[];
+  selectAssetClass: (index: number) => void;
 }) {
-    return (
-        <div className="dropdown-daten">
-            <div className="eingabe-title">
-                <label>Anlageklasse</label>
-                <div className="underLine"></div>
-            </div>
-            <Select options={props.options} defaultValue={props.options[0]} onChange={selectedOption => {
-                if (selectedOption) {
-                    props.selectAssetClass(+selectedOption.value)
-                }
-            }} />
-        </div>
-    );
+  return (
+    <div className="asset-item">
+      <div className="eingabe-title">
+        <label>Anlageklasse</label>
+      </div>
+      <div className="dropdown">
+        <Select
+          options={props.options}
+          defaultValue={props.options[0]}
+          onChange={(selectedOption) => {
+            if (selectedOption) {
+              props.selectAssetClass(+selectedOption.value);
+            }
+          }}
+        />
+      </div>
+    </div>
+  );
 }
--- src/components/pages/Zukäufe.tsx
+++ src/components/pages/Zukäufe.tsx
@@ -2,61 +2,79 @@ import { useState } from "react";
 import { Calculator, Snapshot } from "wpk";
 import AssetClassesSelect from "../AssetClassSelect";
 
-export type OptionType = { label: string, value: string }
+export type OptionType = { label: string; value: string };
 
 export default function Zukäufe(props: {
-  setSimulationOutput: React.Dispatch<React.SetStateAction<any>>,
-  snapshot: Snapshot
+  setSimulationOutput: React.Dispatch<React.SetStateAction<any>>;
+  snapshot: Snapshot;
 }) {
-
   const [options] = useState<OptionType[]>(() => {
-    let temp = ([{ value: '-1', label: 'keine Angabe' }])
+    let temp = [{ value: "-1", label: "keine Angabe" }];
     for (let i = 0; i < props.snapshot.getUserAssetClasses().length; i++) {
-      temp = [...temp, { value: (i + 1).toString(), label: props.snapshot.getUserAssetClasses()[i].title }]
+      temp = [
+        ...temp,
+        {
+          value: (i + 1).toString(),
+          label: props.snapshot.getUserAssetClasses()[i].title,
+        },
+      ];
     }
-    return temp
-  })
+    return temp;
+  });
 
-  const [volume, setVolume] = useState<number>(0)
-  const [index, setIndex] = useState<number>(0)
+  const [volume, setVolume] = useState<number>(0);
+  const [index, setIndex] = useState<number>(0);
 
   const selectAssetClass = (index: number) => {
-    setIndex(index)
-    updateVolume(volume)
-  }
+    setIndex(index);
+    updateVolume(volume);
+  };
 
   const updateVolume = (newVolume: number) => {
-    setVolume(newVolume)
-    console.log(volume) // TODO warum ist volume immernoch der alte Wert
+    setVolume(newVolume);
+    console.log(volume); // TODO warum ist volume immernoch der alte Wert
     //TODO Bug bei Beleihungswert, 7700 statt 770
-    let result
+    let result;
     if (index === -1) {
-      result = Calculator.siumulate(props.snapshot, { 'volume': newVolume }, 'handel')
+      result = Calculator.siumulate(
+        props.snapshot,
+        { volume: newVolume },
+        "handel"
+      );
     } else {
-      result = Calculator.siumulate(props.snapshot, { 'volume': newVolume, 'assetClassIndex': index }, 'handel')
+      result = Calculator.siumulate(
+        props.snapshot,
+        { volume: newVolume, assetClassIndex: index },
+        "handel"
+      );
     }
-    props.setSimulationOutput(result)
-  }
+    props.setSimulationOutput(result);
+  };
 
   return (
     <div>
       <div className="containerLeft-header">
         <h3>Zukauf</h3>
       </div>
-      <AssetClassesSelect options={options} selectAssetClass={selectAssetClass}></AssetClassesSelect>
-      <div className="eingabeItem">
-        <div className="eingabe-title">
-          <label>Kaufvolumen</label>
-          <div className="underLine"></div>
+      <div className="containerLeft-input">
+        <div className="eingabeItem">
+          <div className="eingabe-title">
+            <label>Kaufvolumen</label>
+            <div className="underLine"></div>
+          </div>
+          <div className="eingabe-form">
+            <input
+              type="number"
+              placeholder="Kaufvolumen"
+              min="0"
+              onChange={(e) => updateVolume(+e.target.value)}
+            />
+          </div>
         </div>
-        <div className="eingabe-form">
-          <input
-            type="number"
-            placeholder="Kaufvolumen"
-            min="0"
-            onChange={(e) => updateVolume(+e.target.value)}
-          />
-        </div>
+        <AssetClassesSelect
+          options={options}
+          selectAssetClass={selectAssetClass}
+        ></AssetClassesSelect>
       </div>
     </div>
   );
